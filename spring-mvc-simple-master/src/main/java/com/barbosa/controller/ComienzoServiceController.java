bd5d8ffb9911e63f63cb2eb30ebf6ed7381f7f19
Siddharth Shrivastava <siddhartha.s@UG-BLR-L013.ugamsolutions.com>
2014-01-02 00:46:01 +0530

    other controller still not working:(

--- spring-mvc-simple-master/src/main/java/com/barbosa/controller/ComienzoServiceController.java
+++ spring-mvc-simple-master/src/main/java/com/barbosa/controller/ComienzoServiceController.java
@@ -5,6 +5,7 @@ package com.barbosa.controller;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -12,14 +13,14 @@ import org.springframework.web.bind.annotation.RequestMapping;
  * @author siddhartha.s
  *
  */
-@RequestMapping("start")
+@Controller
 public class ComienzoServiceController {
 	
 	Logger log = LoggerFactory.getLogger(SampleController.class);
-	@RequestMapping("home")
+	@RequestMapping("start")
 	public String loadHomePage(Model m) {
 		log.debug("llllll",m);
-		m.addAttribute("name", "CodeTutr");
+		m.addAttribute("comienzo", "start");
 		return "Welcome";
 	}
 }
